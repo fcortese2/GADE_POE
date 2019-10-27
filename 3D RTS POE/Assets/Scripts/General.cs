9cf6eccc5b38cceb0bff1faa7e3a2428cdc08fc3
fcortese2 <fcortese.design@gmail.com>
2019-10-28 00:01:55 +0200

    V2.0
    Finished game.

--- 3D RTS POE/Assets/Scripts/General.cs
+++ 3D RTS POE/Assets/Scripts/General.cs
@@ -7,15 +7,41 @@ public class General : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
+        
     }
 
-    // Update is called once per frame
+    public GameObject wave1Red;
+    public GameObject wave2Red;
+
+    public GameObject blueKnights;
+    public GameObject redKnights;
+
+    public GameObject gate;
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    Cursor.lockState = CursorLockMode.None;
-        //}
+        if (wave1Red.transform.childCount <= 2)
+        {
+            Debug.Log("wave 2");
+            for (int i = 0; i < wave2Red.transform.childCount; i++)
+            {
+                wave2Red.transform.GetChild(i).GetComponent<RangedUnit>().enabled = true;
+            }
+            wave2Red.GetComponent<RangedUnit>().enabled = true;
+        }
+        if (wave2Red.transform.childCount == 0)
+        {
+            Object.Destroy(gate.gameObject);
+
+            for (int i = 0; i < blueKnights.transform.childCount; i++)
+            {
+                blueKnights.transform.GetChild(i).GetComponent<RangedUnit>().enabled = true;
+            }
+
+            for (int i = 0; i < redKnights.transform.childCount; i++)
+            {
+                redKnights.transform.GetChild(i).GetComponent<RangedUnit>().enabled = true;
+            }
+
+        }
     }
 }
